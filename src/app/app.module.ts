33c103360a307ff25d63679c65c3510dd7caa0c3
aakash14goplani <goplaniaakash14@gmail.com>
2019-11-11 01:39:15 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnauthHeaderComponent } from './unauth-header/unauth-header.component';
+import { AuthHeaderComponent } from './auth-header/auth-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnauthHeaderComponent,
+    AuthHeaderComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
