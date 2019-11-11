744d935ecc79eee2ea61e32e4bbf1f0a08f552f5
Goplani <aakash.goplani@accenture.com>
2019-11-11 21:19:53 +0530

    adding skeleton - 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomePageComponent } from './home-page/home-page.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { AccessibilityStatementComponent } from './accessibility-statement/accessibility-statement.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent },
+  {path: 'home', component: HomePageComponent },
+  {path: 'index', component: HomePageComponent },
+  {path: 'privacy-policy', component: PrivacyPolicyComponent },
+  {path: 'legal-notice', component: LegalNoticeComponent },
+  {path: 'accessibility', component: AccessibilityStatementComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { UnauthHeaderComponent } from './unauth-header/unauth-header.component';
 import { AuthHeaderComponent } from './auth-header/auth-header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { AccessibilityStatementComponent } from './accessibility-statement/accessibility-statement.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { HomePageComponent } from './home-page/home-page.component';
     UnauthHeaderComponent,
     AuthHeaderComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    PrivacyPolicyComponent,
+    LegalNoticeComponent,
+    AccessibilityStatementComponent
   ],
   imports: [
     BrowserModule,
