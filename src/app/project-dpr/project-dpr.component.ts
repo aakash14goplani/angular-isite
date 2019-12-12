f6f371dbffa056058bf38efa513f281ff98aaed7
Goplani <aakash.goplani@accenture.com>
2019-12-12 21:30:08 +0530

    updating DPR

--- src/app/project-dpr/project-dpr.component.ts
+++ src/app/project-dpr/project-dpr.component.ts
@@ -13,79 +13,88 @@ export class ProjectDprComponent implements OnInit {
     private projectDPRService: ProjectDprService
   ) { }
 
-  private projectDPRArray: Array<{date: Date, category: string, type: string, content: number}>;
-  private laboursArray: Array<{date: Date, category: string, type: string, content: number}>;
-  private machineryArray: Array<{date: Date, category: string, type: string, content: number}>;
-  private materialArray: Array<{date: Date, category: string, type: string, content: number}>;
-  private activitiesArray: Array<{date: Date, category: string, type: string, content: number}>;
+  private projectDPRArray: Array<{date: Date, content: string}>;
+  private tempMap = new Map<Date, Array<{content: string}>>();
+  private newProjectDate: Date = new Date();
 
   private isDataUpdating: boolean = false;
-
-  private laboursLastUpdatedDate: Date = new Date('2019-4-14');
-  private machineryLastUpdatedDate: Date = new Date('2019-5-15');
-  private materialLastUpdatedDate: Date = new Date('2019-6-16');
-  private activitiesLastUpdatedDate: Date = new Date('2019-7-17');
+  private isDataAdding: boolean = false;
+  private arrayToAddDPRNotes: number[] = [1];
 
   ngOnInit(): void {
     this.projectDPRArray = this.projectDPRService.getProjectDPRData();
 
     this.projectDPRArray.sort((a, b) => {
       if (a.date > b.date) {
-        return 1;
+        return -1;
       }
       if (a.date < b.date) {
-        return -1;
+        return 1;
       }
       return 0;
     });
 
-    let tempMap = new Map<Date, Array<Array<{date: Date, category: string, type: string, content: number}>>>();
-    let tempArray: Array<Array<{date: Date, category: string, type: string, content: number}>> = [];
-    let currentDate: Date = this.projectDPRArray[0].date;
-    tempArray.push(this.projectDPRArray);
-    tempMap.set(currentDate, tempArray);
-    let nextDate: Date = currentDate;
-    for (let i = 1; i < this.projectDPRArray.length; i++) {
-      nextDate = this.projectDPRArray[i].date;
-      if (nextDate === currentDate) {
-        // get key with existing date, append array to value
+    let tempArray = [];
+    let currentDate: Date = new Date(this.projectDPRArray[0].date);
+    let nextDate: Date = new Date(this.projectDPRArray[1].date);
+
+    for (let i = 1; i <= this.projectDPRArray.length - 1; i++) {
+      tempArray.push(this.projectDPRArray[i].content);
+
+      if (currentDate.getTime() !== nextDate.getTime()) {
+        this.tempMap.set(currentDate, tempArray);
+        tempArray = [];
+      }
+
+      currentDate = new Date(this.projectDPRArray[i].date);
+      if ((i+1) < this.projectDPRArray.length) {
+        nextDate = new Date(this.projectDPRArray[i+1].date);
       } else {
-        // insert new k-v pair
+        nextDate = new Date(this.projectDPRArray[i].date);
       }
     }
 
-
-    this.laboursArray = this.projectDPRArray.filter((data) => {
-      return data.category === 'Labour';
-    });
-    this.machineryArray = this.projectDPRArray.filter((data) => {
-      return data.category === 'Machinery';
-    });
-    this.materialArray = this.projectDPRArray.filter((data) => {
-      return data.category === 'Material';
-    });
-    this.activitiesArray = this.projectDPRArray.filter((data) => {
-      return data.category === 'Activity';
-    });
-
-    console.log('this.projectDPRArray: ', this.projectDPRArray);
-    console.log('this.laboursArray: ', this.laboursArray);
-    console.log('this.machineryArray: ', this.machineryArray);
-    console.log('this.materialArray: ', this.materialArray);
-    console.log('this.activitiesArray: ', this.activitiesArray);
+    tempArray.push(this.projectDPRArray[this.projectDPRArray.length - 1].content);
+    this.tempMap.set(currentDate, tempArray);
   }
 
   private updateProjectDPR(): void {
     const date: Date = new Date();
-    const category: string = '';
-    const type: string = '';
-    const content: number = -1;
-    this.projectDPRService.setProjectDPRData(date, category, type, content);
+    const content: string = '';
+    this.projectDPRService.setProjectDPRData(date, content);
     this.isDataUpdating = true;
   }
 
   private cancelUpdate(): void {
     this.isDataUpdating = false;
+  }
+
+  private maintainOrder(a, b): number {
+    return 0;
+  }
+
+  private addDPRReport(): void {
+    this.isDataAdding = true;
+    let variable: string = 'aakash';
+  }
+
+  private addDPRNotes(): void {
+    this.arrayToAddDPRNotes.push(1);
+  }
+
+  private cancelCurrentNote(index: number): void {
+    // console.log('remove element from position: ' + index);
+    // console.log('remove element: ' + this.arrayToAddDPRNotes.indexOf(index));
+    console.log(this.arrayToAddDPRNotes);
+    this.arrayToAddDPRNotes.splice(index, 1);
+  }
+
+  private cancelDataAddition(): void {
+    this.isDataAdding = false;
+  }
+
+  private addNewDPR(): void {
+    this.isDataAdding = false;
   }
 
 }
--- src/app/project-dpr/project-dpr.service.ts
+++ src/app/project-dpr/project-dpr.service.ts
@@ -5,25 +5,30 @@ export class ProjectDprService {
 
   constructor() { }
 
-  private projectDPRDataStotre: Array<{date: Date, category: string, type: string, content: number}> = [
-    { date: new Date('2017-7-10'), category: 'Labour', type: 'Male', content: 1 },
-    { date: new Date('2018-8-18'), category: 'Labour', type: 'Female', content: 2 },
-    { date: new Date(), category: 'Labour', type: 'NW', content: 3 },
-    { date: new Date('2017-7-10'), category: 'Machinery', type: 'Poclain', content: 1 },
-    { date: new Date('2018-8-18'), category: 'Machinery', type: 'Dumper', content: 2 },
-    { date: new Date(), category: 'Machinery', type: 'JCB', content: 3 },
-    { date: new Date('2018-8-18'), category: 'Material', type: 'Murum', content: 1 },
-    { date: new Date(), category: 'Material', type: 'RMC M20', content: 2 },
-    { date: new Date('2017-7-10'), category: 'Activity', type: 'Earthwork filing', content: -1 },
-    { date: new Date('2018-8-18'), category: 'Activity', type: 'Rock breaking', content: -1 },
-    { date: new Date(), category: 'Activity', type: 'SWD Wall', content: -1 }
+  private projectDPRDataStotre: Array<{date: Date, content: string}> = [
+    { date: new Date('2019-7-10'), content: 'Concreting PCC for swd at ch 23575 to 23607 RHS' },
+    { date: new Date('2019-7-11'), content: 'Concreting swd wall at ch 24410 to 2442, 24764 to 24775 RHS and 24373 LHS' },
+    { date: new Date('2019-7-10'), content: 'Concreting PCC for swd at ch 24835 to 24859 LHS, 24795 to 24800 and  24437 to 24481 RHS' },
+    { date: new Date('2019-7-9'), content: 'Shuttering for swd slab at ch 24169 to 24198 LHS in progress' },
+    { date: new Date('2019-7-8'), content: 'cutting bending steel for swd slab' },
+    { date: new Date('2019-7-10'), content: 'Concreting swd slab at ch 24198 to 24227 RHS' },
+    { date: new Date('2019-7-8'), content: 'Concreting for Encasing HP culvert at ch 18620 RHS  1st lift' },
+    { date: new Date('2019-7-9'), content: 'Concreting PCC for utility duct at ch 19260' },
+    { date: new Date('2019-7-11'), content: 'Concreting swd wall at ch 24421 to 24432 RHS and 24530 to 24539 LHS ' },
+    { date: new Date('2019-7-12'), content: 'Concreting for Encasing HP culvert 1st lift ch 18860 LHS' },
+    { date: new Date('2019-7-12'), content: 'Soling for HP culvert at ch 24955' },
+    { date: new Date('2019-7-11'), content: 'Soling for swd at ch 24830 to 24860 LHS.' },
+    { date: new Date('2019-7-9'), content: 'Embankment grading and watering at ch 24750 to 24950.' },
+    { date: new Date('2019-7-8'), content: 'Dressing excavated bed for HP culvert at ch 25060' },
+    { date: new Date('2019-7-8'), content: 'Dressing excavated swd slab' },
+    { date: new Date('2019-6-8'), content: 'Dressing excavated bed for HP culvert at ch 25060' }
   ];
 
-  public getProjectDPRData(): Array<{date: Date, category: string, type: string, content: number}> {
+  public getProjectDPRData(): Array<{date: Date, content: string}> {
     return this.projectDPRDataStotre.slice();
   }
 
-  public setProjectDPRData(date: Date, category: string, type: string, content: number): void {
-    this.projectDPRDataStotre.push({date: date, category: category, type: type, content: content});
+  public setProjectDPRData(date: Date, content: string): void {
+    this.projectDPRDataStotre.push({date: date, content: content});
   }
 }
